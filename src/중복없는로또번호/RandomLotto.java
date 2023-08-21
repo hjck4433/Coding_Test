4f927fd1afb02aa274d97556c62dd7642f5eee9d
hjck4433 <hjck4433@gmail.com>
2023-08-21 22:50:25 +0900

    Coding Test Practice 20230821

--- src/중복없는로또번호/RandomLotto.java
+++ src/중복없는로또번호/RandomLotto.java
@@ -6,4 +6,21 @@ package 중복없는로또번호;
 // 배열은 6개
 // 반복횟수는 정할 수 없음 (while(true) ==> 탈출 조건 이 있어야 함 (중복되지 않은 번호 6개가 저장되면 break)
 public class RandomLotto {
+    public static void main(String[] args) {
+        int[] lotto = new int[6];
+        boolean isTrue = false;
+        int lottoIndex = 0;
+
+        while (true){
+            int tmp = (int) ((Math.random() * 45) + 1);
+            for (int i = 0; i < lotto.length; i++) {
+                if(lotto[i] == tmp) isTrue = true;
+            }
+            if(!isTrue) lotto[lottoIndex++] = tmp;
+            isTrue = false;
+            if(lottoIndex == 6) break;
+        }
+
+        for(int e : lotto) System.out.print(e + " ");
+    }
 }
