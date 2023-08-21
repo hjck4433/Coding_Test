65c905cd67aff8e7ea8fefa951de48a1f5bec435
hjck4433 <hjck4433@gmail.com>
2023-08-21 17:51:45 +0900

    20230821 Coding Test new test Push

---
+++ src/대소문자바꾸기/ChangeCharCase.java
@@ -0,0 +1,6 @@
+package 대소문자바꾸기;
+// 영어 소문자와 대문자로 이루어진 단어를 입력받은 뒤, 대문자는 소문자로, 소문자는 대문자로 바꾸어 출력하는 프로그램을 작성하시오.
+// charAt(index) : 문자열에 해당 인덱스 문자를 추출
+// toCharArray() : 문자열을 무자 배열로 변환
+public class ChangeCharCase {
+}
