500f5f0ddd2f122006216fd711c096c34349f733
hjck4433 <hjck4433@gmail.com>
2023-08-24 22:05:18 +0900

    Add more Test to solve

---
+++ src/N_집주소/Address.java
@@ -0,0 +1,12 @@
+package N_집주소;
+
+// 1 = 2 / 2 = 3 / 3...0 = 4
+// 숫자를 입력
+// 해당숫자의 몇 칸을 차지하는 지를 출력하는 문제
+// 0을 입력하면 종료
+
+public class Address {
+    public static void main(String[] args) {
+
+    }
+}
