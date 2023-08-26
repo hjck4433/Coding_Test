2f9ccb9ee5a516852a6a158f2713d61723cb0c75
hjck4433 <hjck4433@gmail.com>
2023-08-26 22:37:29 +0900

    add new coding test 20230826

---
+++ src/N_저항/OhmEx.java
@@ -0,0 +1,21 @@
+package N_저항;
+
+// 저항 값 구하기 : 컬러 문자열 3개를 입력 받음
+// 첫번째 컬러 문자열은 10의 자리 수
+// 두번째 컬리 문자열은 1의 자리 수
+// 세번째 컬러 문자열은 곱하는 수
+// black 0 1
+// brown 1 10
+// red   2 100
+// orange 3 1,000
+// yellow 4 10,000
+// green 5 100,000
+// blue 6 1,000,000
+// violet 7 10,000,000
+// grey 8 100,000,000
+// white 9 1,000,000,000
+public class OhmEx {
+    public static void main(String[] args) {
+
+    }
+}
