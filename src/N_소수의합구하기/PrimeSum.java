6f15f375c8c263cc8a4354f616ab1c44caa3d926
hjck4433 <hjck4433@gmail.com>
2023-08-29 08:20:50 +0900

    Solved Adding Prime Num 20230829

--- src/N_소수의합구하기/PrimeSum.java
+++ src/N_소수의합구하기/PrimeSum.java
@@ -1,8 +1,25 @@
 package N_소수의합구하기;
 
+import java.util.Scanner;
+
 // 입력 받은 정수보다 작은 소수의 합을 구하는 함수를 만드세요.
 // 예) 12이면 2+3+5+7+11 = 28;
 // 소수란? 1과 자기 자신 이외의 자연수로 나누어 지지 않는 수
 public class PrimeSum {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정수 입력 : ");
+        int num = sc.nextInt();
+        int sum = 0;
+        for(int i = 2; i < num ; i++) {
+            int n = 0;
+            for(int j = 2; j < i; j++) {
+                if (i % j == 0) n++;
+            }
+            if (n > 0) n = 0;
+            else if(n == 0) n = i;
+            sum += n;
+        }
+        System.out.println(sum);
+    }
 }
