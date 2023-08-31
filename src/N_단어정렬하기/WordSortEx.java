eced2a8ea124cb57693dc1386f5509e6e0bbe49a
hjck4433 <hjck4433@gmail.com>
2023-08-31 22:47:25 +0900

    Adjust WordSortEx 20230831

--- src/N_단어정렬하기/WordSortEx.java
+++ src/N_단어정렬하기/WordSortEx.java
@@ -5,8 +5,39 @@ package N_단어정렬하기;
 //2. 길이가 같으면 사전 순으로
 //3. 중복 제거
 
-public class WordSortEx {
-    public static void main(String[] args) {
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.TreeSet;
 
+public class WordSortEx {
+    // comparator hasset hashmap 차이?
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("N : ");
+        int n = sc.nextInt(); // 단어의 개수
+        TreeSet<Word> words = new TreeSet<>(new CompareWord());
+
+        for(int i = 0; i < n ; i++) { // n의 수 만큼
+            words.add(new Word(sc.next())); // HashSet에 추가
+        }
+
+        for(Word e: words) System.out.print(e.words + " ");
+    }
+}
+class Word {
+    String words;
+    Word(String words) {
+        this.words = words;
+    }
+}
+
+class CompareWord implements Comparator<Word> {
+
+    @Override
+    public int compare(Word o1, Word o2) {
+        if(o1.words.length() > o2.words.length()) return 1;
+        else if (o1.words.length() == o2.words.length()) {
+            return o1.words.compareTo(o2.words);
+        }else return -1;
     }
 }
