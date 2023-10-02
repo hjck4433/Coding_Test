26cbecd760f1da4ed25b234d43226a6b41342261
hjck4433 <hjck4433@gmail.com>
2023-10-02 19:21:48 +0900

    added Baek+2903

---
+++ src/N_Baek_2903/Main.java
@@ -0,0 +1,11 @@
+package N_Baek_2903;
+// 1 => 9 // 5 => 1089
+// 1 ... 3 // 2 ... 5 // 3 ... 9 // 4 ... 17 // 5 ... 33 // 6 ... 65
+// +2 // +3 // +6 // +13 // +28 // + 59
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
