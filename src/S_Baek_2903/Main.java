d215a8171a37f04fb3e0eb076e69c9f018f11a36
hjck4433 <hjck4433@gmail.com>
2023-10-02 19:28:33 +0900

    Solved Baek_2903

--- src/S_Baek_2903/Main.java
+++ src/S_Baek_2903/Main.java
@@ -1,4 +1,4 @@
-package N_Baek_2903;
+package S_Baek_2903;
 // 1 => 9 // 5 => 1089
 // 1 ... 3 // 2 ... 5 // 3 ... 9 // 4 ... 17 // 5 ... 33 // 6 ... 65
 // +2 // +3 // +6 // +13 // +28 // + 59
@@ -7,5 +7,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int dot = 2;
+        for(int i = 1; i <= n ; i++) {
+            dot = (dot * 2) - 1;
+        }
+        System.out.println(dot * dot);
     }
 }
