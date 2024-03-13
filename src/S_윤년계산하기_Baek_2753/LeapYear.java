1e67acd46975e8258fe8c5c3f5fd14dce76544e9
hjck4433 <hjck4433@gmail.com>
2024-03-13 23:13:16 +0900

    added baeknum

--- src/S_윤년계산하기_Baek_2753/LeapYear.java
+++ src/S_윤년계산하기_Baek_2753/LeapYear.java
@@ -1,4 +1,4 @@
-package S_윤년계산하기;
+package S_윤년계산하기_Baek_2753;
 import java.util.Scanner;
 // 연도가 4로 나누어 떨어 진다
 // 100으로 나누어 떨어지면 연도는 윤년이 아니다.
@@ -9,7 +9,7 @@ public class LeapYear {
         System.out.print("연도를 입력 : ");
         int year = sc.nextInt();
 
-        if((year % 4 == 0 || year % 400 == 0) && year % 100 != 0) System.out.println("윤년입니다.");
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) System.out.println("윤년입니다.");
         else System.out.println("윤년이 아닙니다.");
     }
 }
