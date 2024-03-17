450dcae0166cf4e20029295bbdeea9d996a744bb
hjck4433 <hjck4433@gmail.com>
2024-03-17 19:47:18 +0900

    solved Baek2438

---
+++ src/S_Baek_2438/Main.java
@@ -0,0 +1,16 @@
+package S_Baek_2438;
+
+//첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i<=n; i++){
+            System.out.println("*".repeat(i));
+        }
+    }
+}
