0198316bacbaf4e58e5ece5de424dde1d147d030
hjck4433 <hjck4433@gmail.com>
2024-03-31 19:30:30 +0900

    solved Baek 11718

---
+++ src/S_Baek_11718/Main.java
@@ -0,0 +1,25 @@
+package S_Baek_11718;
+
+// 입력이 주어진다. 입력은 최대 100줄로 이루어져 있고,
+// 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다. 각 줄은 100글자를 넘지 않으며,
+// 빈 줄은 주어지지 않는다. 또, 각 줄은 공백으로 시작하지 않고, 공백으로 끝나지 않는다.
+
+// 입력받은 그대로 출력한다.
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str;
+        while((str = br.readLine()) != null){
+            bw.write(str+"\n");
+        }
+        bw.close();
+        br.close();
+    }
+}
