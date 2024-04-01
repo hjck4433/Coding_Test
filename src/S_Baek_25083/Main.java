adbaa8674ad7ce6e8218171ccf99e1484d977a9b
hjck4433 <hjck4433@gmail.com>
2024-04-01 20:38:19 +0900

    solved Baek 25083

---
+++ src/S_Baek_25083/Main.java
@@ -0,0 +1,14 @@
+package S_Baek_25083;
+
+public class Main {
+    public static void main(String[] args){
+        String leaf = "         ,r'\"7\n" +
+                      "r`-_   ,'  ,/\n" +
+                      " \\. \". L_r'\n" +
+                      "   `~\\/\n" +
+                      "      |\n" +
+                      "      |";
+
+        System.out.println(leaf);
+    }
+}
